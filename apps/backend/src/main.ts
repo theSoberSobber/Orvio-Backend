c14b5832cf30799a5e8b172ba2344199dad0c8e4
theSoberSobber <pavitchhabra1611@gmail.com>
2025-03-16 17:36:07 +0530

    [Chore] Enable CORS for SSL orvio.pavit.xyz

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,6 +7,13 @@ async function bootstrap() {
   // TODO: global validation pipe
   // app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors({
+    origin: 'https://orvio.pavit.xyz',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   app.useGlobalFilters(new RpcToHttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
